71e51c9757242e82a6260fb74f3b080cf1dc4c02
ChwgithubHZ <905489472cc@sina.com>
2018-10-19 18:01:27 +0800

    v1

---
+++ src/main/java/com/ljnet/VueManager/VueManagerApplication.java
@@ -0,0 +1,12 @@
+package com.ljnet.VueManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueManagerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ljnet/VueManager/VueManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ljnet.VueManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VueManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
